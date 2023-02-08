da7ab9a747f1066dbd1328177af0efc9e74793d8
FiddleComputers <Tonin386@gmail.com>
2023-02-08 03:48:32 +0100

    First functionnal model for Monster implemented

--- entities/Monster.cpp
+++ entities/Monster.cpp
@@ -1,12 +1,54 @@
 #include "Monster.hpp"
+#include <string>
 
-Monster::Monster() {}
+using namespace std;
 
-Monster::Monster(int a, int h, int mh) : _atk(a), _hp(h) , _maxhp(mh) {}
-
-void Monster::attack(Monster* target)
+Monster::Monster()
 {
-    target->_hp -= _atk;
+    _atk = 1000;
+    _hp = 10000;
+    _maxHp = 10000;
+    _speed = 100;
+    _maxStamina = 1000;
+    _stamina = 0;
+
+    _name = "Frog";
+    _rarity = 1;
+
+    _armor = new Item();
+    _weapon = new Item();
+    _ring1 = new Item();
+    _ring2 = new Item();
+    _talisman = new Item();
+}
+
+Monster::Monster(double atk, double mh, double speed, double ms, double s, string n, int r)
+    : _atk(atk), _maxHp(mh), _speed(speed), _maxStamina(ms), _stamina(s), _name(n), _rarity(r)
+{
+    _hp = _maxHp;
+
+    _armor = new Item();
+    _weapon = new Item();
+    _ring1 = new Item();
+    _ring2 = new Item();
+    _talisman = new Item();
+}
+
+void Monster::attack(Monster *target, double bonusStam)
+{
+    target->receiveDamage(_atk);
+    _stamina = bonusStam;
+}
+
+void Monster::prepare(double modifier)
+{
+    _stamina += _speed * modifier;
+}
+
+void Monster::receiveDamage(double d)
+{
+    double damage = d * getDefenseRate();
+    _hp -= damage;
 }
 
 bool Monster::isAlive() const
@@ -14,14 +56,126 @@ bool Monster::isAlive() const
     return _hp >= 0;
 }
 
-int Monster::getHp() const
+bool Monster::canPlay() const
+{
+    return _stamina > _maxStamina;
+}
+
+void Monster::equipArmor(Item *a)
+{
+    delete _armor;
+    _armor = a;
+}
+
+void Monster::equipWeapon(Item *w)
+{
+    delete _weapon;
+    _weapon = w;
+}
+
+void Monster::equipRing1(Item *r)
+{
+    delete _ring1;
+    _ring1 = r;
+}
+
+void Monster::equipRing2(Item *r)
+{
+    delete _ring2;
+    _ring2 = r;
+}
+
+void Monster::equipTalisman(Item *t)
+{
+    delete _talisman;
+    _talisman = t;
+}
+
+double Monster::getCritRate() const
+{
+    // TODO
+    return 0;
+}
+
+double Monster::getDodgeRate() const
+{
+    // TODO
+    return 0;
+}
+
+double Monster::getComboRate() const
+{
+    // TODO
+    return 0;
+}
+
+double Monster::getStunRate() const
+{
+    // TODO
+    return 0;
+}
+
+double Monster::getDefenseRate() const
+{
+    // TODO
+    return .1;
+}
+
+double Monster::getHp() const
 {
     return _hp;
 }
 
-int Monster::getMaxHp() const
+double Monster::getMaxHp() const
 {
-    return _maxhp;
+    return _maxHp;
+}
+
+double Monster::getAttack() const
+{
+    return _atk;
+}
+
+double Monster::getMaxStamina() const
+{
+    return _maxStamina;
+}
+
+double Monster::getStamina() const
+{
+    return _stamina;
+}
+
+double Monster::getSpeed() const
+{
+    return _speed;
+}
+
+string Monster::getName() const
+{
+    return _name;
+}
+
+string Monster::getRarity() const
+{
+    switch (_rarity)
+    {
+    case 1:
+        return "Common";
+        break;
+    case 2:
+        return "Uncommon";
+        break;
+    case 3:
+        return "Rare";
+        break;
+    case 4:
+        return "Epic";
+        break;
+    case 5:
+        return "Legendary";
+        break;
+    }
 }
 
 Monster::~Monster()
--- entities/Monster.hpp
+++ entities/Monster.hpp
@@ -1,23 +1,64 @@
 #ifndef MONSTER_HPP
 #define MONSTER8HPP
 
+#include "items/Item.hpp"
+
 class Monster
 {
 protected:
-    int _atk;
-    int _hp;
-    int _maxhp;
+    double _atk;
+    double _hp;
+    double _maxHp;
+    double _speed;
+    double _maxStamina;
+    double _stamina;
+
+    std::string _name;
+    int _rarity;
+
+    Item* _armor;
+    Item* _weapon;
+    Item* _ring1;
+    Item* _ring2;
+    Item* _talisman;
 
 public:
     Monster();
-    Monster(int a, int h, int mh);
+    Monster(double atk, double mh, double speed, double ms, double s, std::string n, int r);
 
-    virtual void attack(Monster *target);
+    /* TURN RELATED FUNCTIONS */
+    virtual void attack(Monster *target, double bonusStam = 0);
+    virtual void prepare(double modifier = 1);
+    virtual void receiveDamage(double d);
 
     virtual bool isAlive() const;
+    virtual bool canPlay() const;
 
-    virtual int getHp() const;
-    virtual int getMaxHp() const;
+    /* STUFF RELATED FUNCTIONS */
+    virtual void equipArmor(Item* a);
+    virtual void equipWeapon(Item* w);
+    virtual void equipRing1(Item* r);
+    virtual void equipRing2(Item* r);
+    virtual void equipTalisman(Item* t);
+
+    /* DAMAGE DONE MODIFIERS */
+    virtual double getCritRate()    const;
+    virtual double getDodgeRate()   const;
+    virtual double getComboRate()   const;
+    virtual double getStunRate()    const;
+
+    /* INCOMING DAMAGE MODIFIERS */
+    virtual double getDefenseRate() const;
+
+    /* SETTERS & GETTERS OF ATTRIBUTES */
+    virtual double      getHp()         const;
+    virtual double      getMaxHp()      const;
+    virtual double      getAttack()     const;
+    virtual double      getMaxStamina() const;
+    virtual double      getStamina()    const;
+    virtual double      getSpeed()      const;
+    virtual std::string getName()       const;
+    virtual std::string getRarity()     const;
 
     ~Monster();
 };
